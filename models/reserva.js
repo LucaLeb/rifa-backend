d6eb394ec45342cab5e0df71ee133b2017a30b18
LucaLeb <cristianlucasdlp@hotmail.com>
2025-07-06 14:42:42 -0300

    Update reserva.js

--- models/reserva.js
+++ models/reserva.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const reservaSchema = new mongoose.Schema({
-    nombre: String,
-    dni: String,
-    numeros: [Number],
-    estado: { type: String, default: "pendiente"},
-    mp_preference_id: String
+  nombre: String,
+  dni: String,
+  numeros: [Number],
+  estado: { type: String, default: "pendiente" },
+  mp_preference_id: String
 });
 
-export default mongoose.model("reserva". reservaSchema);
+export default mongoose.model('reserva', reservaSchema);
